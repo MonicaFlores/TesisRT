8613d48cb7a70665a7eb1a25147ff2a7fe18efde
rtruffel <45605499+rtruffel@users.noreply.github.com>
2019-07-19 17:06:47 -0400

    Cambios laptop RT

--- VecindadTopologica/1_Delaunay.R
+++ VecindadTopologica/1_Delaunay.R
@@ -1,5 +1,5 @@
 
-#install.packages(c("tripack", "spdep", "RANN", "foreach", "rgdal"))
+install.packages(c("tripack", "spdep", "RANN", "foreach", "rgdal"))
 
 library(tripack)
 library(spdep)
@@ -12,15 +12,15 @@ library(glue)
 options(scipen=999)
 
 # setwd("/Users/MoniFlores/Desktop/Tesis RT/Data")
-setwd("C:/Users/CEDEUS 18/Documents/CEDEUS/Monica - 2018/15_TesisRT/Data")
-setwd
+setwd("C:/Users/CEDEUS 18/Documents/CEDEUS/Monica - 2018/15_TesisRT/Data") #desktop Monica
+setwd("C:/Users/rtruf/Desktop/WORK/TESIS_DISC/DATA") #laptop Ricardo
 
 
 ####### Triangulation [Creacion de Vecinos]  #####
 
 comunas <- c(13101, 13102, 13103, 13104, 13105, 13106, 13107, 13108, 13109, 13110, 13111, 13112, 13113, 13114,
              13115, 13116, 13117, 13118, 13119, 13120, 13121, 13123, 13124, 13125, 13126, 13127, 13128, 13130,
-             13131, 13132, 13201, #13203, San José de Maipo falla - solo tiene 3 manzanas con POB válida dentro del AUC
+             13131, 13132, 13201, #13203, San Jose de Maipo falla - solo tiene 3 manzanas con POB Valida dentro del AUC
              13301, 13302, 13401, 13403, 13601, 13604, 13605, 13122, 13129)
 
 for (comuna in comunas) {
@@ -43,13 +43,13 @@ for (comuna in comunas) {
   
   # Vecindad original
   vecs1 <- tri2nb(coords, row.names = IDs) # Delauney Triangulation - generar vecinos
-  plot(vecs1, coords) # Plotear triangulación
+  plot(vecs1, coords) # Plotear triangulacion
   
   # Fijar conjunto de vecinos ------------------------------------------
   
-  metros <- 900 # Definir distancia máxima entre vecinos en metros
+  metros <- 900 # Definir distancia maxima entre vecinos en metros
   
-  # Función obtiene vecinos que esten a menos de 900 metros ó un mínimo de 3 vecinos
+  # Funcion obtiene vecinos que esten a menos de 900 metros o un minimo de 3 vecinos
   thresh <- foreach(v = 1:length(vecs1)) %do%  {
   
     # Extraer coordenadas
@@ -57,35 +57,35 @@ for (comuna in comunas) {
     neighs <- coords[vecs1[[v]],] # Coordenadas de cada vecino
     x <- neighs[1,] # Coordenadas de cada vecino
   
-    # Aplicar función cálculo de distancia a cada vecino de cada punto
+    # Aplicar funcion calculo de distancia a cada vecino de cada punto
     e <- apply(neighs,1, function(x) {
       d <- sqrt((x[1] - source[1])^2 + (x[2] - source[2])^2) # Distancia en metros
       d
     })
   
-    # Ordenar y encontrar vecinos mínimos y máximos
+    # Ordenar y encontrar vecinos minimos y maximos
     orden <- e[order(e)] # Ordenar por distancia
-    minimos <- orden[1:2] # Obtener los 2 vecinos más cercanos
-    maximos <- orden[1:min(3,length(e))] # Obtener los vecinos más lejanos
+    minimos <- orden[1:2] # Obtener los 2 vecinos mcs cercanos
+    maximos <- orden[1:min(3,length(e))] # Obtener los vecinos mas lejanos
   
-   # Vecino en menos de 900 metros ó dentro del conjunto de mínimos y máximos
+   # Vecino en menos de 900 metros o dentro del conjunto de minimos y maximos
     f <- unlist(lapply(1:length(e), function(x) { # asigna False
-      (e[x] < metros | # distancia menor a máximo en metros ó
-        names(e[x]) %in% names(minimos)) & # Vecinos en el conjunto de mínimo Y
-        names(e[x]) %in% names(maximos) # Vecinos en el cojunto de máximos
+      (e[x] < metros | # distancia menor a maximo en metros o
+        names(e[x]) %in% names(minimos)) & # Vecinos en el conjunto de minimo Y
+        names(e[x]) %in% names(maximos) # Vecinos en el cojunto de maximos
     }))
   
   }
   
-  # Aplicar función thresh a listado de vecinos
+  # Aplicar funcion thresh a listado de vecinos
   vecs <- vecs1 # Definir objeto vecs
   for (obs in 1:length(vecs1)) {
     vecs[[obs]] <- vecs[[obs]] [thresh[[obs]]]
   }
   
-  plot(vecs,coords) # Plotear triangulación final
+  plot(vecs,coords) # Plotear triangulacion final
   
-  # Traspasar vecindad a líneas
+  # Traspasar vecindad a lineas
   lw <- nb2listw(vecs) # Spatial Weights For Neighbours Lists
   lineas <- listw2lines(lw, coords, proj4string = CRS(proj4string(ciudad))) # Use Arc-Type Shapefiles For Import And Export Of Weights
   # plot.nb(vecs, coords) # Plotear
